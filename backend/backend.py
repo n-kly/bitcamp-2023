d33869589bf45ce6ed17c92a5487a5c7c024d983
Akshay Trivedi <25870016+SnootierMoon@users.noreply.github.com>
2023-04-08 19:23:55 -0400

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -25,7 +25,7 @@ async def gpt(location, avg_sun, avg_precipitation, avg_temperature, avg_humidit
         engine="text-davinci-003",
          prompt =f"""
 What are the best crops to grow in{location}given the following factors: the average precipitation is {avg_precipitation}mm, the average temperature is {avg_temperature}F, the average humidity is {avg_humidity}%, and the average sun coverage is {avg_sun}%.
-Provide the response as a JSON object with two fields: "crops" and "buddy". The "crops" field should be an array of 5 objects representing the top crops to plant. Each object should have a field called "name" with the name of the crop, and a field called "description" with a unique description of why to plant the crop. The "buddy" field should have some fun facts about the crop and the location.
+Provide the response as a JSON object with two fields: "crops" and "buddy". The "crops" field should be an array of 5 objects representing the top crops to plant. Each object should have a field called "name" with the name of the crop, and a field called "description" with a unique description of why to plant the crop. The "buddy" field should have some fun facts about the crop and gardening.
 """,
         max_tokens=2000
     )
