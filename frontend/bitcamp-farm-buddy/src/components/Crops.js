0a9f1c8e9a9697aabf70775014c3628577c693e9
Nirav Koley <niravkoley@gmail.com>
2023-04-09 02:56:12 -0400

    chagnedda

--- frontend/bitcamp-farm-buddy/src/components/Crops.js
+++ frontend/bitcamp-farm-buddy/src/components/Crops.js
@@ -11,7 +11,7 @@ const Crops = ({ results }) => {
                 {crops ? crops.map(crop => <SingleCrop crop={crop} />) 
                     : 
                     <div class="loading">
-                        <img alt="Loading..." className="loading-img" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/35771931234507.564a1d2403b3a.gif" />
+                        <img alt="Loading..." className="loading-img" src="/25.gif" />
                     </div>}
             </div>
         </div>
