0b9a90fabb22012b07b59ee65a355030b9ce2675
Nirav Koley <niravkoley@gmail.com>
2023-04-09 00:16:44 -0400

    stuff

--- frontend/bitcamp-farm-buddy/src/components/SpeechBubble.js
+++ frontend/bitcamp-farm-buddy/src/components/SpeechBubble.js
@@ -1,4 +1,4 @@
-
+import './styles.css';
 
 const SpeechBubble = ({results}) => {
     const buddy = results.buddy;
@@ -6,8 +6,12 @@ const SpeechBubble = ({results}) => {
     
     return (
         <div className="speech-container">
-            <div className="speech-bubble">
-                <p>{buddy}</p>
+            <div className="bubble bubble-bottom-left">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+                </p>
             </div>
             
         </div>
